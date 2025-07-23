fc92d442d11e18d05eec3b62adf5b4f03ef3bf18
Vunhan1002 <vunhan10022004@gmail.com>
2025-07-23 09:43:44 +0700

    Complete exercise 2

--- app/src/main/java/com/example/androidmobileapplicationpractice/MainActivity.java
+++ app/src/main/java/com/example/androidmobileapplicationpractice/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.androidmobileapplicationpractice;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,25 +14,55 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    //Khai báo các biến giao diện tại đây
-    EditText edtA, edtB, edtTotal;
-    Button btnSum;
+    Button btncall;
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "CR424 - onDestroy()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "CR424 - onPause()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "CR424 - onRestart()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "CR424 - onResume()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "CR424 - onStart()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "CR424 - onStop()", Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Ánh xạ các biến giao diện
-        edtA = findViewById(R.id.edtA);
-        edtB = findViewById(R.id.edtB);
-        edtTotal = findViewById(R.id.edtTotal);
-        btnSum = findViewById(R.id.btnSum);
-        btnSum.setOnClickListener(new View.OnClickListener() {
+        Toast.makeText(this, "CR424 - onCreat()", Toast.LENGTH_SHORT).show();
+        btncall = findViewById(R.id.btncall);
+        btncall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int a = Integer.parseInt(edtA.getText().toString());
-                int b = Integer.parseInt(edtB.getText().toString());
-                int c = a + b;
-                edtTotal.setText(c+"");
+                Intent intent1 = new Intent(MainActivity.this, Subactivity.class);
+                startActivity(intent1);
             }
         });
     }
