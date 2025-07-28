7be6b66a8dc51de53321b5d45562d0ff14a83388
Vunhan1002 <vunhan10022004@gmail.com>
2025-07-28 21:49:36 +0700

    Complete Exercise 8(1)

--- app/src/main/java/com/example/androidmobileapplicationpractice/MainActivity.java
+++ app/src/main/java/com/example/androidmobileapplicationpractice/MainActivity.java
@@ -4,60 +4,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    EditText edtA, edtB, edtKQ;
-    Button btnrequest;
+    Button btnCall, btnSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        edtA = findViewById(R.id.edtA);
-        edtB = findViewById(R.id.edtB);
-        edtKQ = findViewById(R.id.edtKQ);
-        btnrequest = findViewById(R.id.btnrequest);
+        btnCall = findViewById(R.id.btnCall);
+        btnSend = findViewById(R.id.btnSend);
 
-        btnrequest.setOnClickListener(new View.OnClickListener() {
+        btnCall.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                // Tạo intent
-                Intent myintent = new Intent(MainActivity.this, ResultActivity.class);
-
-                // Lấy dữ liệu
-                int a = Integer.parseInt(edtA.getText().toString());
-                int b = Integer.parseInt(edtB.getText().toString());
-
-                // Truyền dữ liệu
-                myintent.putExtra("soa", a);
-                myintent.putExtra("sob", b);
-
-                // Gửi và chờ kết quả trả về
-                startActivityForResult(myintent, 99);
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CallPhoneActivity.class);
+                startActivity(intent);
             }
         });
-    }
 
-    // Nhận kết quả trả về
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == 99) {
-            if (resultCode == 33) {
-                int sum = data.getIntExtra("kq", 0);
-                edtKQ.setText("Tổng 2 số là: " + sum);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SendSMSActivity.class);
+                startActivity(intent);
             }
-            if (resultCode == 34) {
-                int sub = data.getIntExtra("kq", 0);
-                edtKQ.setText("Hiệu 2 số là: " + sub);
-            }
-        }
+        });
     }
 }
