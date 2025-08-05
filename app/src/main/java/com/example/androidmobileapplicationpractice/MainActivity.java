6bc1d3dd99329e0a03213dc6fddd7b92be0b238f
Vunhan1002 <vunhan10022004@gmail.com>
2025-08-05 23:21:05 +0700

    Complete Exercise 11

--- app/src/main/java/com/example/androidmobileapplicationpractice/MainActivity.java
+++ app/src/main/java/com/example/androidmobileapplicationpractice/MainActivity.java
@@ -1,23 +1,32 @@
 package com.example.androidmobileapplicationpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import android.Manifest;
-import android.content.pm.PackageManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editTextInput;
+    private Button btnGoToSecond;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Kiểm tra và yêu cầu quyền
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_SMS, Manifest.permission.READ_SMS}, 1);
-        }
+        editTextInput = findViewById(R.id.editTextInput);
+        btnGoToSecond = findViewById(R.id.btnGoToSecond);
+
+        btnGoToSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputText = editTextInput.getText().toString();
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("userInput", inputText);
+                startActivity(intent);
+            }
+        });
     }
 }
